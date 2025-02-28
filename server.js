244cf8886fb04cf99ede3e4beeb00ce23936ee5e
BssCreate <bed_random@mail.ru>
2025-03-01 00:35:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Раздача статических файлов (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, "public")));
 
-// Обработчик главной страницы
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Запуск сервера
+// Обработчик 404 (если страница не найдена)
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
